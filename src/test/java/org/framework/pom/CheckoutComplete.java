c09cb5a049d7b23993c74913a2db75adb87f4a9b
George Gregory <georgegregory@live.co.uk>
2022-01-19 16:20:16 +0000

    Created checkoutComplete POM

---
+++ src/test/java/org/framework/pom/CheckoutComplete.java
@@ -0,0 +1,18 @@
+package org.framework.pom;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class CheckoutComplete {
+    WebDriver driver;
+    private By back = new By.ById("back-to-products");
+
+    public CheckoutComplete(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    public Products goToPastPage(){
+        driver.findElement(back).click();
+        return new Products(driver);
+    }
+}
