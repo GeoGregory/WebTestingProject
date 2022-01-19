132eeecfa3a7a319761b12116c0f905384fda4b9
GeoGregory <91059962+GeoGregory@users.noreply.github.com>
2022-01-19 16:34:09 +0000

    Update CheckoutComplete.java

--- src/test/java/org/framework/pom/CheckoutComplete.java
+++ src/test/java/org/framework/pom/CheckoutComplete.java
@@ -4,11 +4,11 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
 public class CheckoutComplete extends PageWithHeaderAndFooter {
-    WebDriver driver;
+
     private By back = new By.ById("back-to-products");
 
     public CheckoutComplete(WebDriver driver) {
-        this.driver = driver;
+        super(driver);
     }
 
     public Products goToPastPage(){
