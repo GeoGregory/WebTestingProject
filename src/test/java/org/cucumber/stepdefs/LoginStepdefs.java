d9ccb5f1be8013914d333f4469bc15aecc572c85
Louis C-H <louisclementharris@yahoo.co.uk>
2022-01-20 11:08:30 +0000

    setup Cucumber files

--- src/test/java/org/cucumber/stepdefs/LoginStepdefs.java
+++ src/test/java/org/cucumber/stepdefs/LoginStepdefs.java
@@ -1,4 +1,55 @@
-package test.java.org.cucumber.stepdefs;
+package org.cucumber.stepdefs;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
 
 public class LoginStepdefs {
+    @Given("I have a valid username and password")
+    public void iHaveAValidUsernameAndPassword() {
+    }
+
+    @When("I type both in and press login")
+    public void iTypeBothInAndPressLogin() {
+    }
+
+    @Then("I should be logged in and moved to the main page")
+    public void iShouldBeLoggedInAndMovedToTheMainPage() {
+    }
+
+    @When("I type both in and press enter")
+    public void iTypeBothInAndPressEnter() {
+    }
+
+    @Given("that my username or password are invalid")
+    public void thatMyUsernameOrPasswordAreInvalid() {
+    }
+
+    @Then("an error should be displayed letting me know they are invalid")
+    public void anErrorShouldBeDisplayedLettingMeKnowTheyAreInvalid() {
+    }
+
+    @When("I leave the username field blank")
+    public void iLeaveTheUsernameFieldBlank() {
+    }
+
+    @Then("an error should be displayed letting me know what fields I didn't fill out")
+    public void anErrorShouldBeDisplayedLettingMeKnowWhatFieldsIDidnTFillOut() {
+    }
+
+    @When("I leave the password field blank")
+    public void iLeaveThePasswordFieldBlank() {
+    }
+
+    @Given("that I trigger an error message")
+    public void thatITriggerAnErrorMessage() {
+    }
+
+    @When("I click the x on the error message")
+    public void iClickTheXOnTheErrorMessage() {
+    }
+
+    @Then("the error message should go away")
+    public void theErrorMessageShouldGoAway() {
+    }
 }
