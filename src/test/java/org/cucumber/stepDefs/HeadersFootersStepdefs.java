9d6bf6049d7d1f1872ea053d7ff6b314bf52f076
George Gregory <georgegregory@live.co.uk>
2022-01-20 13:48:20 +0000

    Small fix

--- src/test/java/org/cucumber/stepDefs/HeadersFootersStepdefs.java
+++ src/test/java/org/cucumber/stepDefs/HeadersFootersStepdefs.java
@@ -3,6 +3,7 @@ package org.cucumber.stepDefs;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import org.framework.pom.IndividualProduct;
 import org.framework.pom.LoginPage;
 import org.framework.pom.Products;
 import org.openqa.selenium.WebDriver;
@@ -12,24 +13,24 @@ public class HeadersFootersStepdefs {
 
     private WebDriver driver;
     private LoginPage loginPage;
-    private Products products;
+    private Products productsPage;
+    private IndividualProduct productPage;
 
     @Given("I am on a page with a header and footer")
     public void iAmOnAPageWithAHeaderAndFooter() {
         System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
         driver = new ChromeDriver();
         loginPage = new LoginPage(driver);
-//        loginPage.inputLoginFields("standard_user", "secret_sauce");
-//        loginPage.clickLoginButton();
-
-        products = loginPage.quickLogin();
+        productsPage = loginPage.quickLogin();
     }
 
     @When("I click on the burger menu")
     public void iClickOnTheBurgerMenu() {
+
     }
 
     @Then("The burger menu with display its elements")
     public void theBurgerMenuWithDisplayItsElements() {
+        System.out.println("hi");
     }
 }
