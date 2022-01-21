041924fac92263a1f7cb6001ed9fc986ef31d396
TWParsons <TParsons@SpartaGlobal.com>
2022-01-21 12:19:45 +0000

    What a ballache. feature file and step defs for CheckoutOverview CheckoutOverview class extended with a method that returns the subtotal and total

--- src/test/java/framework/pom/CheckoutOverview.java
+++ src/test/java/framework/pom/CheckoutOverview.java
@@ -1,5 +1,6 @@
-package org.framework.pom;
+package test.java.framework.pom;
 
+import io.cucumber.java.eo.Do;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -14,7 +15,7 @@ public class CheckoutOverview extends PageWithHeaderAndFooter{
 
     private final By finish = new By.ById("finish");
     private final By cancel = new By.ById("cancel");
-    private final By cartItems = new By.ByClassName("cart_list");
+    private final By cartItems = new By.ByClassName("cart_items");
     private final By subTotal = new By.ByClassName("summary_subtotal_label");
     private final By tax = new By.ByClassName("summary_tax_label");
     private final By total = new By.ByClassName("summary_total_label");
@@ -36,7 +37,15 @@ public class CheckoutOverview extends PageWithHeaderAndFooter{
         return driver.findElement(subTotal).getText();
     }
 
+    public double getMoneyAsDouble(String value) {
+        String amount[] = value.split(" ");
+        return Double.parseDouble(amount[1].substring(1));
+    }
 
+    public double getSubtotalAsDouble(){
+        String[] amount = getSubtotal().split(" ");
+        return Double.parseDouble(amount[2].substring(1));
+    }
 
     public String getTax(){
         return driver.findElement(tax).getText();
@@ -45,4 +54,6 @@ public class CheckoutOverview extends PageWithHeaderAndFooter{
     public String getTotal(){
         return driver.findElement(total).getText();
     }
+
+
 }
--- src/test/resources/features/CheckoutOverview.feature
+++ src/test/resources/features/CheckoutOverview.feature
@@ -9,10 +9,18 @@ Feature: As a user, I want to know the total cost of my cart, so I can know how 
 
     Given I am on the CheckoutOverview Page
 
-  Scenario: I want to see the total of my purchases
-    When I check the total of my purchases
-    Then They should be calculated together with tax added on
+ Scenario: I want to see the total of my purchases
+    When I check the <total> of my purchases
+    Then It should be the <subtotal> plus the <tax>
 
-  Scenario: I want to see the added tax
-    When I check the tax
-    Then I see what the tax is
+   Scenario: I want to go back to products
+     When I click the cancel button
+     Then I go back to the products page
+
+     Scenario: I want to go to finish my order
+       When I click the finish button
+       Then I go to the Checkout Complete Page
+
+       Scenario: I want to see the products I am purchasing
+         When I look at my items
+         Then I see all the things I am purchasing
